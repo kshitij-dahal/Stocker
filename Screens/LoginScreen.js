2084603066a99722a167a0ef702cec6e1aac7502
Kshitij Dahal <kshitijdahal314@gmail.com>
2020-12-31 15:53:13 -0500

    add basic login screen with functionless button

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Button title="Login With WealthSimple" />
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+// stack navigator for clicking on stocks
+// when navigating can just pass in param to route
+// check if user is logged in from checking storage for user token
+// use context to pass data down to componets at many nesting levels
+// props.chihldren can be used to have a border around stuff
+
+// for cleaner code if you need to pass down something that is only needed
+// at the deepest level, pass down the ocmponent itself and not liike avatar
+// size or sth so that it is not known what exactly is passed down
+
