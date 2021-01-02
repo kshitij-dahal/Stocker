499a3008f9b48f188965836e53a4d2a45dfeb35f
Shree Shah <shreeshah@Shrees-MacBook-Pro.local>
2021-01-02 17:49:28 -0500

    UI enhancement for login and otp screen

--- App.js
+++ App.js
@@ -2,11 +2,44 @@ import React from 'react';
 import LoginScreen from './Screens/LoginScreen';
 import StockListScreen from './Screens/StockListScreen';
 import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {AuthContext} from './AuthContext';
+import {ThemeContext} from './ThemeContext';
+
 import OTPScreen from './Screens/OTPScreen';
 import {loginUser} from './APIConnectors/WealthSimpleConnector';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'rgb(61, 75, 86)',
+    primaryText: 'rbg(255, 255, 255)',
+    secondaryText: 'rbg(0, 0, 0)',
+    secondaryBackground: 'rbg(211, 211, 211)'
+  },
+  view: {
+    width: "100%",
+    height: "10%",
+    margin: 'auto',
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  linearGradient: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    height: "100%",
+    width: "100%",
+  },
+};
 
 const Stack = createStackNavigator();
 
@@ -51,10 +84,13 @@ const App = () => {
     },
   );
 
-  React.useEffect(async () => {
-    try {
-      let token = JSON.parse(await AsyncStorage.getItem('tokens'));
-    } catch (e) {}
+  React.useEffect(() => {
+    async function asyncStorage() {
+      try {
+        let token = JSON.parse(await AsyncStorage.getItem('tokens'));
+      } catch (e) {}
+    }
+    asyncStorage();
   }, []);
 
   const authContext = {
@@ -98,20 +134,22 @@ const App = () => {
   };
 
   return (
-    <AuthContext.Provider value={authContext}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={() => {
-            return state.userAccessToken == null ? 'Login' : 'StockList';
-          }}>
-          {Object.entries(
-            state.userAccessToken == null ? screens.auth : screens.user,
-          ).map(([name, component]) => (
-            <Stack.Screen name={name} component={component} />
-          ))}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthContext.Provider>
+    <ThemeContext.Provider value={theme}>
+      <AuthContext.Provider value={authContext} theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName={() => {
+              return state.userAccessToken == null ? 'OTP' : 'StockList';
+            }}>
+            {Object.entries(
+              state.userAccessToken == null ? screens.auth : screens.user,
+            ).map(([name, component]) => (
+              <Stack.Screen name={name} component={component} />
+            ))}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthContext.Provider>
+      </ThemeContext.Provider>
   );
 };
 
--- Screens/OTPScreen.js
+++ Screens/OTPScreen.js
@@ -1,38 +1,70 @@
 import React from 'react';
-import {View, TextInput, Button, Alert} from 'react-native';
+import {View, TextInput, TouchableOpacity, Alert, Text, StyleSheet} from 'react-native';
 import {AuthContext} from '../AuthContext';
+import {ThemeContext} from '../ThemeContext';
+import LinearGradient from 'react-native-linear-gradient';
+import { buttons, text, inputBox } from './styles'
+
 
 const OTPScreen = ({navigation}) => {
   const [otp, setOtp] = React.useState('');
 
   return (
-    <View>
-      <TextInput
-        secureTextEntry
-        autoCompleteType="off"
-        autoCorrect={false}
-        style={{borderBottomWidth: 2}}
-        onChangeText={setOtp}
-      />
-      <AuthContext.Consumer>
-        {(data) => (
-          <Button
-            title="Continue W OTP"
-            onPress={async () => {
-              let result = await data.signInWithOtp(otp);
-              if (!result) {
-                Alert.alert(
-                  'Incorrect OTP',
-                  'Please enter the correct OTP sent to your Email',
-                  [{text: 'OK'}],
-                );
-              }
-            }}
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <View style={theme.background}>
+          <LinearGradient
+            colors={[theme.colors.background, '#979899']}
+            style={theme.linearGradient}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+        >
+          <TextInput
+            style={[inputBox.textBox, styles.otpBox]}
+            secureTextEntry
+            autoCompleteType="off"
+            autoCorrect={false}
+            onChangeText={setOtp}
+            keyboardType="phone-pad"
+            textAlign="center"
+            maxLength={6}
+            underlineColorAndroid='white'
+            justifyContent='space-evenly'
           />
-        )}
-      </AuthContext.Consumer>
-    </View>
+          <AuthContext.Consumer>
+            {(data) => (
+              <View style={theme.view}>
+                <TouchableOpacity 
+                  style={buttons.wealthsimpleButton}
+                  onPress={async () => {
+                    let result = await data.signInWithOtp(otp);
+                    if (!result) {
+                      Alert.alert(
+                        'Incorrect OTP',
+                        'Please enter the correct OTP sent to your Email',
+                        [{text: 'OK'}],
+                      );
+                    }
+                  }}
+                >
+                  <Text style={text.buttonText}>Continue with OTP</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </AuthContext.Consumer>
+        </LinearGradient>
+      </View>
+    )}
+    </ThemeContext.Consumer>
   );
 };
+
+const styles = StyleSheet.create({
+  otpBox: {
+    fontSize: 24,
+    justifyContent: 'center',
+    letterSpacing: 30
+  }
+});
 
 export default OTPScreen;
